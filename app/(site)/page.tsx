c5c20b3a438b79767d786b74a9007c7af8c83f50
nazhang8888 <nazhang@pm.me>
2023-12-04 21:27:32 -0500

    feature:header & library, supporting code

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
-  return <div className="text-green-500">Main Content</div>;
+  return (
+    <div
+      className="
+      bg-neutral-900
+      rounded-lg
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-scroll
+      "
+    >
+      <Header>Header</Header>
+    </div>
+  );
 }
