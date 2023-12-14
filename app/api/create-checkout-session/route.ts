e6fdca28001f52e9b77d83b0d500282eaa022f44
nazhang8888 <nazhang@pm.me>
2023-12-13 20:11:31 -0500

    fix: api route

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -36,8 +36,6 @@ export async function POST(request: Request) {
       mode: "subscription",
       allow_promotion_codes: true,
       subscription_data: {
-        // stripe api has changed, find alternative if necessary
-        // trial_from_plan: true,
         metadata,
       },
       success_url: `${getURL()}/account`,
